e2af7db215ed514fe4d61787a03ab2a32d335b7e
Sergey Romanov <xxsmotur@gmail.com>
2015-06-14 22:59:22 +0500

    Add first test

---
+++ redislist_test.go
@@ -0,0 +1,17 @@
+package redisvars
+
+import
+(
+	"testing"
+)
+
+func TestSetInList(t *testing.T) {
+	rv := New("localhost:6379")
+	lst := rv.NewList()
+	lst.SetList("A", []string{"A","B","C"})
+	lst.CommitList()
+	result := lst.GetList("A")
+	if result[0] != "C" || result[1] != "B" || result[2] != "A"{
+		t.Errorf("Not match")
+	}
+}
